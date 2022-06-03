a656f8560bed77b65ba289b20f2b99f170d000ed
bundoski <93097556+bundoski@users.noreply.github.com>
2022-06-03 03:16:38 +0200

    Add files via upload

---
+++ Lab3_Blog_App/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Lab3BlogAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Lab3_Blog_App'
---
+++ Lab3_Blog_App/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+class Profile(models.Model):
+    first_name=models.CharField(max_length=50, null=True, blank=True)
+    last_name=models.CharField(max_length=50, null=True, blank=True)
+    age=models.PositiveIntegerField(null=True, blank=True)
+    hobbies=models.TextField(null=True, blank=True)
+    user=models.ForeignKey(User, on_delete=models.CASCADE, primary_key=True, unique=True)
+
+    def __str__(self):
+        return self.first_name+" "+self.last_name + " (@"+self.user.username+")";
+
+class Post(models.Model):
+    title=models.CharField(max_length=50, null=True, blank=True)
+    author=models.ForeignKey(Profile, on_delete=models.CASCADE)
+    content=models.TextField(null=True, blank=True)
+    date_created=models.DateTimeField(null=True, blank=True)
+    last_modified=models.DateTimeField(null=True, blank=True)
+
+class File(models.Model):
+    file=models.FileField()
+    post=models.ForeignKey(Post,on_delete=models.CASCADE, null=True, blank=True)
+
+class Block(models.Model):
+    blocker=models.ForeignKey(Profile,on_delete=models.CASCADE,related_name='+', null=True, blank=True)
+    blocked=models.ForeignKey(Profile,on_delete=models.CASCADE,related_name='+', null=True, blank=True)
+
+class Comment(models.Model):
+    content=models.TextField()
+    comment_author=models.ForeignKey(Profile,on_delete=models.CASCADE, null=True,blank=True)
+    post=models.ForeignKey(Post,on_delete=models.CASCADE)
+
+
+
